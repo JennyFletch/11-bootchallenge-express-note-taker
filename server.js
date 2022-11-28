0020c126f3d6b90597d94568d4cd831695d9a287
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-11-28 15:08:08 -0700

    Got routes and paths working correctly.

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const PORT = 3001;
+const path = require('path');
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files to client
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// GET Route for homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+
+
+
+
+
+
+
+
+
+/* const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+ */
